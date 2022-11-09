b4a3edf40cb3518fe01ec1e4340bedbcbd501186
riyajath-ahamed <riyajatha@gmail.com>
2022-11-10 03:32:25 +0530

    Debug img

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -17,13 +17,13 @@ const [index, setIndex] = useState(0);
     <div>
       <div className='product-detail-container'>
         <div>
-          {/* <div className='image-container'>
+          <div className='image-container'>
             <img src={urlFor(image && image[index])} 
             className="product-detail-image"
             />
 
           </div>
-          <div className= 'small-images-container'>
+          {/* <div className= 'small-images-container'>
             {image?.map ((item, i) =>(
               <img src= {urlFor(item)} 
               className={i === index ? 'small-image selected-image' : 'small-image'}
